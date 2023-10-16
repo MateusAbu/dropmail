087497e14d90de0197de263ba7400d8528cb6b1a
Mateus Abu Kamel <88752743+MateusAbu@users.noreply.github.com>
2023-10-16 20:09:48 -0300

    bug: Fix import

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import Header from './components/Header'
-import Inbox from './components/Inbox'
+import Header from './components/Header/Header'
+import Inbox from './components/Inbox/Inbox'
 
 function App() {
 
